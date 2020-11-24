e029da5bb534765b500d1d4e7285f1decb1620cf
ESGit3 <ethanshan706@gmail.com>
2020-11-24 07:36:26 -0500

    equals and tester

---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester {
+    public static void main(String[] args) {
+        RealNumber a = new RealNumber(4.0);
+        RealNumber b = new RealNumber(4.0);
+
+        System.out.println(a.equals(b));
+    }
+}
