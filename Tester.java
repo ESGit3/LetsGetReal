0effb7c4ed5ed1003ae57d0c9ce54f46d2a8a3e6
ESGit3 <ethanshan706@gmail.com>
2020-11-24 07:37:29 -0500

    add

--- Tester.java
+++ Tester.java
@@ -1,8 +1,9 @@
 public class Tester {
     public static void main(String[] args) {
         RealNumber a = new RealNumber(4.0);
-        RealNumber b = new RealNumber(4.0);
+        RealNumber b = new RealNumber(34.723572);
 
         System.out.println(a.equals(b));
+        System.out.println(a.add(b));
     }
 }
